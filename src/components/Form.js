919b980b519e7d904dda58baed4ff54b24ca163f
OmariBennett <omari.bennett@gmail.com>
2021-09-15 18:27:40 -0400

    Refactor code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
-const formValid = (formErrors) => {
+const emailRegex = RegExp(
+	/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+);
+
+const formValid = ({ formErrors, ...rest }) => {
 	let valid = true;
 
+	// validate form errors being empty
 	Object.values(formValid).forEach((val) => val.length > 0 && (valid = false));
+
+	// validate the form was filled out
+	Object.values(rest).forEach((val) => val === null && (valid = false));
 	return valid;
 };
 
@@ -28,7 +36,7 @@ export default class Form extends Component {
 	handleSubmit = (e) => {
 		e.preventDefault();
 
-		if (formValid(this.state.formErrors)) {
+		if (formValid(this.state)) {
 			console.log(`
         --SUBMITTING--
         firstName: ${this.state.firstName}
@@ -41,22 +49,58 @@ export default class Form extends Component {
 		}
 	};
 
+	handleChange = (e) => {
+		e.preventDefault();
+
+		const { name, value } = e.target;
+		let formErrors = { ...this.state.formErrors };
+
+		switch (name) {
+			case 'firstName':
+				formErrors.firstName =
+					value.length < 3 ? 'minimum 3 characaters required' : '';
+				break;
+			case 'lastName':
+				formErrors.lastName =
+					value.length < 3 ? 'minimum 3 characaters required' : '';
+				break;
+			case 'email':
+				formErrors.email = emailRegex.test(value)
+					? ''
+					: 'invalid email address';
+				break;
+			case 'password':
+				formErrors.password =
+					value.length < 6 ? 'minimum 6 characaters required' : '';
+				break;
+			default:
+				break;
+		}
+
+		this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+	};
+
 	render() {
+		const { formErrors } = this.state;
+
 		return (
 			<div className='wrapper'>
 				<div className='form-wrapper'>
 					<h1>Create Account</h1>
-					<form onSubmit={this.handleSubmit} onValidate>
+					<form onSubmit={this.handleSubmit}>
 						<div className='firstName'>
 							<label htmlFor='firstName'>First Name</label>
 							<input
 								type='text'
 								noValidate
-								className=''
+								className={formErrors.firstName.length > 0 ? 'error' : null}
 								placeholder='First Name'
 								name='firstName'
 								onChange={this.handleChange}
 							/>
+							{formErrors.firstName.length > 0 && (
+								<span className='errorMessage'>{formErrors.firstName}</span>
+							)}
 						</div>
 
 						<div className='lastName'>
@@ -64,11 +108,14 @@ export default class Form extends Component {
 							<input
 								type='text'
 								noValidate
-								className=''
+								className={formErrors.lastName.length > 0 ? 'error' : null}
 								placeholder='Last Name'
 								name='lastName'
 								onChange={this.handleChange}
 							/>
+							{formErrors.lastName.length > 0 && (
+								<span className='errorMessage'>{formErrors.lastName}</span>
+							)}
 						</div>
 
 						<div className='email'>
@@ -76,11 +123,14 @@ export default class Form extends Component {
 							<input
 								type='email'
 								noValidate
-								className=''
+								className={formErrors.email.length > 0 ? 'error' : null}
 								placeholder='Email'
 								name='email'
 								onChange={this.handleChange}
 							/>
+							{formErrors.email.length > 0 && (
+								<span className='errorMessage'>{formErrors.email}</span>
+							)}
 						</div>
 
 						<div className='password'>
@@ -88,11 +138,14 @@ export default class Form extends Component {
 							<input
 								type='password'
 								noValidate
-								className=''
+								className={formErrors.password.length > 0 ? 'error' : null}
 								placeholder='Password'
 								name='password'
 								onChange={this.handleChange}
 							/>
+							{formErrors.password.length > 0 && (
+								<span className='errorMessage'>{formErrors.password}</span>
+							)}
 						</div>
 
 						<div className='createAccount'>
